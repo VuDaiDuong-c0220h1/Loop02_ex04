518374026dd542a930be9d515df5dbf281bba8d6
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-02-27 10:55:21 +0700

    Add files via upload

---
+++ loop02_ex04/app.js
@@ -0,0 +1,7 @@
+let a = "X";
+let b = "&ensp;";
+document.write(a.repeat(21) + "<br/>");
+for (i=1; i<=5; i++) {
+    document.write(a + b.repeat(27) + a +"<br/>");
+}
+document.write(a.repeat(21) + "<br/>");
